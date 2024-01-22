df6b53cf1dca130ecefcec357c921f3a0e58bfa1
LassiEH <lassi.haarala@live.fi>
2024-01-22 21:12:59 +0200

    Can now fill the page. Fails when the page has an internal server error

--- tasks.py
+++ tasks.py
@@ -24,9 +24,11 @@ def robot_order():
 
 
 def open_order_website():
+    """Opens the target website"""
     browser.goto("https://robotsparebinindustries.com/#/robot-order")
 
 def get_orders():
+    """Downloads the file we need and saves the contents into a table variable"""
     library = Tables()
     http = HTTP()
     http.download(url = "https://robotsparebinindustries.com/orders.csv", overwrite=True)
@@ -37,22 +39,40 @@ def get_orders():
     return orders
 
 def close_annoying_modal():
+    """Closes the pop up modal"""
     page = browser.page()
     page.click("button:text('OK')")
 
 def loop_orders(orders):
+    """Goes through the orders in the table"""
     for order in orders:
         message = order
         fill_the_order(order)
 
 
 def fill_the_order(order_info):
+    """Fills every slot in the order page with right information"""
     page = browser.page()
+
+    #Inputs the data for the head part
     page.select_option("#head", order_info["Head"])
 
+    #Inputs the data for the body part
     text = "id-body-" + str(order_info["Body"])
     page.click(f"id={text}")
     
+    #Inputs the data for the legs part
     page.fill("//input[@type='number']", order_info["Legs"])
 
+    #Inputs the data for the address
+    page.fill("#address", order_info["Address"])
+
+    page.click("button:text('Order')")
+
+    #if page.locator('role=alert alert-danger'):
+    #    page.click("button:text('Order')")
+
+    page.click("button:text('Order another robot')")
+
+    close_annoying_modal()
 
